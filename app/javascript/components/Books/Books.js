76f62b54a12a9544c0aa0e9ec3314e07da513e77
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2020-08-16 14:29:47 +0530

    :wrench: fix navigation

--- app/javascript/components/Books/Books.js
+++ app/javascript/components/Books/Books.js
@@ -21,7 +21,7 @@ const Books = () => {
 
   return (
     <div>
-      <div class="text-center mb-4">
+      <div className="text-center mb-4">
         <h1>OpenBooks</h1>
         <div>Curates the critical and honest reviews of the best books</div>
       </div>
